77a35d9e59d71adbc624cc31b8615557054437f2
FelixDou <doubletfelix@orange.fr>
2023-12-03 17:19:45 +0900

    Pop up click
    Ça me saoule j'arrive pas à intégrer les caractères non ascii

--- Assets/Scripts/ButtonBook.cs
+++ Assets/Scripts/ButtonBook.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems; // Required for the event trigger interfaces
+using System.Collections; // For coroutines
+using TMPro;
 
 public class ButtonBook : MonoBehaviour, IPointerDownHandler, IPointerUpHandler // Implement these interfaces
 {
@@ -9,6 +11,7 @@ public class ButtonBook : MonoBehaviour, IPointerDownHandler, IPointerUpHandler 
     public Image bookImage; // Reference to the book's Image component
     public float scaleIncrease = 1.1f; // Multiplier by which the book's scale will increase
     private Vector3 originalScale; // To store the original scale
+    public TextMeshProUGUI popUpTextPrefab;
 
     void Start() {
         audioSource = GetComponent<AudioSource>();
@@ -22,12 +25,45 @@ public class ButtonBook : MonoBehaviour, IPointerDownHandler, IPointerUpHandler 
     public void OnPointerDown(PointerEventData eventData) {
         if (bookImage != null) {
             bookImage.rectTransform.localScale = originalScale * scaleIncrease; // Increase the size
+            CreateAndAnimateText(); // Create and animate the text
         }
-        // You can also add your click logic here
+        // Click logic
         GameManager.TotalClicks += GameManager.TotalClicksPerTap;
         audioSource.clip = buttonSound;
         audioSource.Play();
     }
+
+    private void CreateAndAnimateText() {
+        // Instantiate the TextMeshProUGUI prefab at the position of the button, with no rotation, and as a child of the current transform.
+        TextMeshProUGUI popUpTextInstance = Instantiate(popUpTextPrefab, transform.position, Quaternion.identity, transform);
+        popUpTextInstance.text = popUpTextPrefab.text; // Set the text to the prefab's text.
+        StartCoroutine(FadeAndMoveUpText(popUpTextInstance)); // Start the coroutine to fade and move the text.
+    }
+
+    private IEnumerator FadeAndMoveUpText(TextMeshProUGUI textInstance) {
+        float duration = 2.0f; // Duration in seconds for the fade and movement.
+        float elapsedTime = 0;
+        Vector3 startPosition = textInstance.transform.position;
+        float randomXDirection = Random.Range(-1f, 1f); // Get a random direction in X.
+
+        while (elapsedTime < duration) {
+            elapsedTime += Time.deltaTime;
+            // Calculate new alpha based on elapsed time.
+            float newAlpha = Mathf.Clamp01(1 - (elapsedTime / duration));
+            textInstance.color = new Color(textInstance.color.r, textInstance.color.g, textInstance.color.b, newAlpha);
+
+            // Move the text upwards and slightly randomly to the sides.
+            float newY = Mathf.Lerp(startPosition.y, Screen.height, elapsedTime / duration);
+            float newX = startPosition.x + (randomXDirection * elapsedTime * 50); // Randomize the x to create a flying effect.
+            textInstance.transform.position = new Vector3(newX, newY, startPosition.z);
+
+            yield return null;
+        }
+
+        Destroy(textInstance.gameObject); // Destroy the text object after the animation.
+    }
+
+
 
     public void OnPointerUp(PointerEventData eventData) {
         if (bookImage != null) {
@@ -35,3 +71,4 @@ public class ButtonBook : MonoBehaviour, IPointerDownHandler, IPointerUpHandler 
         }
     }
 }
+
