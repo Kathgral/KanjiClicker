03c577408845e0795abc66eac7d35c3275929b5c
MoAzFecO <jules.feron@gmail.com>
2023-12-05 15:55:28 +0900

    Better Start Screen

--- Assets/Scripts/LoadMainGameScene.cs
+++ Assets/Scripts/LoadMainGameScene.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PressToContinue : MonoBehaviour
+public class LoadMainGameScene : MonoBehaviour
 {
     void Update()
     {
         // Check for touch input
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            // Load the main game scene
-            SceneManager.LoadScene("MainGame");
+            SceneManager.LoadSceneAsync("MainGame");
         }
     }
 }
---
+++ Assets/Scripts/TextAnimation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class TextAnimation : MonoBehaviour
+{
+    public TextMeshProUGUI textElement;
+    private float animationDuration = 1f;
+
+    void Start()
+    {
+        StartCoroutine(PeriodicTextAnimation());
+    }
+
+    IEnumerator PeriodicTextAnimation()
+    {
+        while (true)
+        {
+            // Fade out
+            float elapsedTime = 0f;
+
+            // Reset text color and size
+            textElement.transform.localScale = Vector3.one;
+
+            // Pause for a moment before the next animation
+            yield return new WaitForSeconds(1f);
+
+            // Shrink in size
+            elapsedTime = 0f;
+            Vector3 initialScale = textElement.transform.localScale;
+            Vector3 targetScale = Vector3.zero;
+
+            while (elapsedTime < animationDuration)
+            {
+                textElement.transform.localScale = Vector3.Lerp(initialScale, targetScale, elapsedTime / animationDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
